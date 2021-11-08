38500857b0752b5fb824757ef8f65762aaba2c66
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2021-11-08 21:41:24 +0800

    passwdLogin() finish

--- demo.py
+++ demo.py
@@ -11,7 +11,17 @@ def createAccount(account_id, customer_id, currenct_type):
     create_time = create_time = datetime.today().strftime("%Y-%m-%d")
     sql_command = "INSERT INTO Account (account_id, customer_id, create_time, currenct_type) VALUES ('"+account_id+"','"\
                   +customer_id+"','"+create_time+"','"+currenct_type+"');"
-    print(sql_command)
     cursor.do(sql_command)
 
-createAccount('015', '007', 'Pound')
+def passwdLogin(customer_id, password):
+    cursor = my_cursor()
+    sql_command = "SELECT password FROM Customer WHERE customer_id='"+customer_id+"';"
+    response = cursor.do(sql_command)
+    if response[0][0] == password:
+        return True
+    else:
+        return False
+
+# createAccount('015', '007', 'Pound')
+print(passwdLogin('001', 'iamjack'))
+print(passwdLogin('002', 'iamjack'))
